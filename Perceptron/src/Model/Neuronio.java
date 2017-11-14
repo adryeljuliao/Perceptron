f2556c6560e6f7cb1f74aa151b6f4577da3f17e2
DryZão <DryZo@LAPTOP-GRV6853M>
2017-11-13 22:39:13 -0200

    final

--- Perceptron/src/Model/Neuronio.java
+++ Perceptron/src/Model/Neuronio.java
@@ -58,7 +58,7 @@ public class Neuronio {
 
 	}
 
-	
+	 
 	public void setEntradas(int[] entradas) {
 		this.entradas = entradas;
 	}
--- Perceptron/src/Model/Rede.java
+++ Perceptron/src/Model/Rede.java
@@ -9,14 +9,16 @@ public class Rede {
 	int qtdExemplos;
 	Neuronio neuronioMCP;
 
-	int[][] entradas = { {1,0,1,0},{0,1,0,1},{1,0,1,0},{0,1,1,1},{0,0,1,1},{0,0,0,1} };
-	int[] saidaDesejada = {0,1,0,1,0,1};
+	int[][] entradas = { { 0, 1, 0, 1 }, { 1, 0, 1, 0 }, { 0, 1, 1, 1 }, { 0, 0, 1, 1 },
+			{ 0, 0, 0, 1 } };
+	int[] saidaDesejada = { 1, 0, 1, 0, 1 };
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Rede r = new Rede();
 		r.iniciarRede();
 		r.treinar();
+		r.executar();
 	}
 
 	public void iniciarRede() {
@@ -27,7 +29,7 @@ public class Rede {
 
 		qtdExemplos = entradas.length;
 		qtdAtributos = entradas[0].length;
-		
+
 		neuronioMCP = new Neuronio(qtdAtributos);
 
 	}
@@ -37,14 +39,14 @@ public class Rede {
 		int cont = 0;
 		do {
 			System.out.println("-------------------------------------");
-			System.out.println("----------- Interação " + cont +  " -------------");
+			System.out.println("----------- Interação " + cont + " -------------");
 			System.out.println("-------------------------------------");
 			erroTotal = 0;
-			
-			for (int i = 0; i < entradas.length; i++){
-				
+
+			for (int i = 0; i < entradas.length; i++) {
+
 				neuronioMCP.setEntradas(entradas[i]);
-				
+
 				neuronioMCP.calculaSaida();
 				System.out.println("saida: " + neuronioMCP.getSaida());
 				neuronioMCP.setErro(saidaDesejada[i]);
@@ -56,15 +58,26 @@ public class Rede {
 		} while (erroTotal != 0);
 
 	}
-	public void executar(){
+
+	public void executar() {
 		int op = 0;
-		do{
+		int[] testeEntrada = new int[entradas[0].length];
+
+		System.out.println("\n\nINFORME OS VALORES DA ENTRADA\n");
+		for (int i = 0; i < testeEntrada.length; i++) {
+			testeEntrada[i] = entrada.nextInt();
+
+		}
 			
-			System.out.println("\n\nINFORME A QUANTIDADE DE ENTRADA");
-			
-			
-		}while(op != 0);
 		
-		
+		neuronioMCP.setEntradas(testeEntrada);
+		neuronioMCP.calculaSaida();
+		System.out.println("Saida : " + neuronioMCP.getSaida());
+
+		System.out.println("digite 0 para informar outra entrada: ");
+		op = entrada.nextInt();
+		if(op == 0){
+			executar();
+		}
 	}
 }
